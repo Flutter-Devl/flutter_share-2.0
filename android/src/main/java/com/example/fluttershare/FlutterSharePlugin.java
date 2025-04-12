ebe3001e36e365e0625a7b95492d525f262cf40e
Mehsara Irfan <mehsara.irfan@manafatech.com>
2025-04-12 18:42:41 +0500

    feat: Add Flutter Share plugin for sharing messages, links, and files
    - Implemented web support with index.html and manifest.json for PWA functionality.
    - Created iOS plugin files including FlutterSharePlugin.h, FlutterSharePlugin.m, and SwiftFlutterSharePlugin.swift.
    - Added share and shareFile methods for sharing content from Flutter apps.
    - Updated pubspec.yaml to include plugin configuration and dependencies.
    - Added .gitignore for iOS and Android directories to exclude unnecessary files.
    - Created initial Android module configuration files.
    - Added example usage in lib/flutter_share.dart.

---
+++ android/src/main/java/com/example/fluttershare/FlutterSharePlugin.java
@@ -0,0 +1,130 @@
+package com.example.fluttershare;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.text.TextUtils;
+import android.util.Log;
+
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+
+/** FlutterSharePlugin */
+public class FlutterSharePlugin implements FlutterPlugin, MethodCallHandler {
+    private Context context;
+    private MethodChannel methodChannel;
+
+    public FlutterSharePlugin() {}
+
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+        context = binding.getApplicationContext();
+        BinaryMessenger messenger = binding.getBinaryMessenger();
+        methodChannel = new MethodChannel(messenger, "flutter_share");
+        methodChannel.setMethodCallHandler(this);
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        context = null;
+        methodChannel.setMethodCallHandler(null);
+        methodChannel = null;
+    }
+
+    @Override
+    public void onMethodCall(MethodCall call, Result result) {
+        if (call.method.equals("shareFile")) {
+            shareFile(call, result);
+        } else if (call.method.equals("share")) {
+            share(call, result);
+        } else {
+            result.notImplemented();
+        }
+    }
+
+    private void share(MethodCall call, Result result) {
+        try {
+            String title = call.argument("title");
+            String text = call.argument("text");
+            String linkUrl = call.argument("linkUrl");
+            String chooserTitle = call.argument("chooserTitle");
+
+            if (title == null || title.isEmpty()) {
+                Log.e("FlutterShare", "Title null or empty");
+                result.error("FlutterShare: Title cannot be null or empty", null, null);
+                return;
+            }
+
+            ArrayList<String> extraTextList = new ArrayList<>();
+            if (text != null && !text.isEmpty()) {
+                extraTextList.add(text);
+            }
+            if (linkUrl != null && !linkUrl.isEmpty()) {
+                extraTextList.add(linkUrl);
+            }
+
+            String extraText = TextUtils.join("\n\n", extraTextList);
+
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_SUBJECT, title);
+            intent.putExtra(Intent.EXTRA_TEXT, extraText);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+            Intent chooserIntent = Intent.createChooser(intent, chooserTitle);
+            chooserIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            context.startActivity(chooserIntent);
+
+            result.success(true);
+        } catch (Exception ex) {
+            Log.e("FlutterShare", "Error sharing text", ex);
+            result.error(ex.getMessage(), null, null);
+        }
+    }
+
+    private void shareFile(MethodCall call, Result result) {
+        try {
+            String title = call.argument("title");
+            String text = call.argument("text");
+            String filePath = call.argument("filePath");
+            String fileType = call.argument("fileType");
+            String chooserTitle = call.argument("chooserTitle");
+
+            if (filePath == null || filePath.isEmpty()) {
+                Log.e("FlutterShare", "ShareLocalFile Error: filePath null or empty");
+                result.error("FlutterShare: FilePath cannot be null or empty", null, null);
+                return;
+            }
+
+            File file = new File(filePath);
+            Uri fileUri = FileProvider.getUriForFile(context, context.getPackageName() + ".provider", file);
+
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType(fileType);
+            intent.putExtra(Intent.EXTRA_SUBJECT, title);
+            intent.putExtra(Intent.EXTRA_TEXT, text);
+            intent.putExtra(Intent.EXTRA_STREAM, fileUri);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+            Intent chooserIntent = Intent.createChooser(intent, chooserTitle);
+            chooserIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            context.startActivity(chooserIntent);
+
+            result.success(true);
+        } catch (Exception ex) {
+            Log.e("FlutterShare", "Error sharing file", ex);
+            result.error(ex.getMessage(), null, null);
+        }
+    }
+}
